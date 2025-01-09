41599577fea65bd1530a0cf22acc508c96690e79
zohaib-lashari <zohaiblashari781@gmail.com>
2025-01-10 02:08:35 +0500

    clean project

---
+++ 01component/src/Button.js
@@ -0,0 +1,11 @@
+
+export function Button(){
+    return(
+        <>
+        <h1>hello zohaib</h1>
+        <button className="button">
+            This is button 
+        </button>
+        </>
+    )
+}
